7d7628229a60b8d169bb4dc499b26f485eccf7ed
Ben Hughes <ben@puppetlabs.com>
2012-07-11 16:59:18 -0700

    Graphite moving to gunicorn.
    rename graphiti to graphiti, so it doesn't clash with graphite in nginx.
    Remove apache code out of graphite, and the django that pulls in apache.
    Rip out all the apache code out of graphite.
    Currently uses IP:Port over HTTP, rather than a unix socket, as I found it
    unreliable (I believe due to my initscript) and I just don't have the
    inclination to fix it all (or merge the one I found on github).
    Huge linting changes.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,114 +1,126 @@
 class graphite (
+    $servername = 'graphite.puppetlabs.net',
     $site_alias = $fqdn
   ) {
 
   include graphite::install
   include graphite::params
 
-  $graphitedir  = $graphite::params::graphitedir
-  $graphiteuser = $graphite::params::graphiteuser
+  $graphitedir       = $graphite::params::graphitedir
+  $graphiteuser      = $graphite::params::graphiteuser
+  $graphiteapproot   = "${graphitedir}/webapp/graphite"
+  $graphitewebroot   = "${graphitedir}/webapp"
+  $graphitegunsocket = 'http://127.0.0.1:3232/'
 
+  $graphite_aliases = {
+    '/content/' => "${graphitewebroot}/",
+      '/media/' => '/usr/share/pyshared/django/contrib/admin/'
+  }
 
-  include apache::mod::wsgi
-  include apache::mod::python
-  include django
+  # Replace the need for apache with gunicorn/nginx
+  gunicorn::app { 'graphite':
+    approot         => $graphiteapproot,
+    gunicorn_socket => $graphitegunsocket,
+    require         => User[$graphiteuser],
+  } ->
+  nginx::unicorn { 'graphite':
+      servername     => $servername,
+      port           => 80,
+      unicorn_socket => $graphitegunsocket,
+      path           => $graphiteapproot,
+      aliases        => $graphite_aliases,
+      gunicorn       => true,
+  }
 
-  package { "python-cairo":    ensure => installed; }
-  package { "python-memcache": ensure => installed; }
-  package { "python-sqlite":   ensure => installed; }
-  package { "python-twisted":  ensure => installed; }
-  package { "python-django-tagging":  ensure => installed; }
-  package { "memcached":       ensure => installed; }
+  package { 'python-cairo':           ensure => installed; }
+  package { 'python-memcache':        ensure => installed; }
+  package { 'python-sqlite':          ensure => installed; }
+  package { 'python-twisted':         ensure => installed; }
+  package { 'python-django':          ensure => installed; }
+  package { 'memcached':              ensure => installed; }
+  package { 'python-django-tagging':  ensure => installed; }
+  package { 'python-simplejson':      ensure => installed; }
 
-  user{ $graphiteuser:
-    system     => true,
+  user { $graphiteuser:
     ensure     => present,
+    system     => true,
     gid        => $graphite::params::graphitegroup,
     managehome => false,
     home       => $graphitedir,
     before     => Class['graphite::install'],
   }
 
-  apache::vhost {"$site_alias":
-    port     => '80',
-    docroot  => '/var/www',
-    ssl      => false,
-    priority => 10,
-    template => 'graphite/apache.conf.erb',
-    require  => Exec["install graphite"],
-  }
-
   file { "${graphitedir}/conf/graphite.wsgi":
     source    => "${graphitedir}/conf/graphite.wsgi.example",
     mode      => '0644',
     owner     => $graphiteuser,
-    subscribe => Exec["install graphite"],
-    require   => [ User[$graphiteuser],Exec["install graphite"] ],
+    subscribe => Exec['install graphite'],
+    require   => [ User[$graphiteuser],Exec['install graphite'] ],
   }
 
   file { "${graphitedir}/storage":
     owner     => $graphite::params::web_user,
     group     => $graphite::params::graphitegroup,
     mode      => '0664',
-    subscribe => Exec["install graphite"],
+    subscribe => Exec['install graphite'],
     recurse   => true,
-    require   => [ User[$graphiteuser],Exec["install graphite"] ],
+    require   => [ User[$graphiteuser],Exec['install graphite'] ],
   }
 
   file { "${graphitedir}/conf/carbon.conf":
-    source    => "puppet:///modules/graphite/carbon.conf",
+    source    => 'puppet:///modules/graphite/carbon.conf',
     mode      => '0644',
     owner     => $graphiteuser,
-    subscribe => Exec["install carbon"],
-    require   => [ User[$graphiteuser],Exec["install carbon"] ],
+    subscribe => Exec['install carbon'],
+    require   => [ User[$graphiteuser],Exec['install carbon'] ],
   }
 
   file { "${graphitedir}/conf/storage-schemas.conf":
-    source    => "puppet:///modules/graphite/storage-schemas.conf",
+    source    => 'puppet:///modules/graphite/storage-schemas.conf',
     owner     => $graphiteuser,
     mode      => '0644',
-    subscribe => Exec["install carbon"],
-    require   => [ User[$graphiteuser],Exec["install carbon"] ],
+    subscribe => Exec['install carbon'],
+    require   => [ User[$graphiteuser],Exec['install carbon'] ],
   }
 
   file { "${graphitedir}/conf/dashboard.conf":
-    source    => "puppet:///modules/graphite/dashboard.conf",
+    source    => 'puppet:///modules/graphite/dashboard.conf',
     owner     => $graphiteuser,
     mode      => '0644',
-    subscribe => Exec["install graphite"],
-    require   => [ User[$graphiteuser], Exec["install graphite"] ],
+    subscribe => Exec['install graphite'],
+    require   => [ User[$graphiteuser], Exec['install graphite'] ],
   }
 
-  file{ '/var/log/graphite':
+  file { '/var/log/graphite':
+    ensure => link,
     target => '/opt/graphite/storage/log/',
-    ensure => link
   }
 
-  exec { "remove stale carbon-cache pidfile":
+  exec { 'remove stale carbon-cache pidfile':
     command => "rm ${graphitedir}/storage/carbon-cache-a.pid",
-    path    => ["/usr/bin", "/bin"],
+    path    => ['/usr/bin', '/bin'],
     unless  => "pgrep carbon-cache.py || test ! -f ${graphitedir}/storage/carbon-cache-a.pid",
   }
 
   file { '/etc/init.d/carbon':
-    source    => "puppet:///modules/graphite/carbon_initscript",
+    ensure    => file,
+    source    => 'puppet:///modules/graphite/carbon_initscript',
     owner     => 'root',
     mode      => '0755',
-    ensure    => file,
     require   => User[$graphiteuser],
   }
 
-   service { 'carbon':
-     ensure     => running,
-     enable     => true,
-     hasrestart => true,
-     hasstatus  => true,
-     require    => [
+  service { 'carbon':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    require    => [
       File["${graphitedir}/conf/carbon.conf"],
       File['/etc/init.d/carbon'],
-      Exec["install whisper"],
-      Exec["remove stale carbon-cache pidfile"],
-    ],
+      Exec['install whisper'],
+      Exec['remove stale carbon-cache pidfile'],
+      ],
   }
 
   bacula::job {
@@ -116,15 +128,15 @@ class graphite (
       files => ["${graphitedir}"],
   }
 
-  cron { "remove ancient graphite log files":
-    command => "/usr/bin/find /opt/graphite/storage/log -type f -mtime +14 | /usr/bin/xargs -I {} rm {}",
+  cron { 'remove ancient graphite log files':
+    command => '/usr/bin/find /opt/graphite/storage/log -type f -mtime +14 | /usr/bin/xargs -I {} rm {}',
     user    => root,
     minute  => 18,
     hour    => 4,
     weekday => 3;
   }
 
-  cron { "compress oldish graphite log files":
+  cron { 'compress oldish graphite log files':
     command => '/usr/bin/find /opt/graphite/storage/log -type f -mtime +3 -regex ".*[^.gz$]" | /usr/bin/xargs -I {} /usr/bin/nice -n 19 /usr/bin/ionice -c3 gzip {}',
     user    => root,
     minute  => 41,
